24403b8f6f90dc4f3e28ea61560be2ec61de2f46
tn9th8 <tn9th8@gmail.com>
2025-07-01 10:13:47 +0700

    chore

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -2,7 +2,6 @@
 import { Logger } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { log } from 'console';
 
 type TOrmTypeServer = 'postgres' | 'mysql' | 'mssql';
 
@@ -19,8 +18,11 @@ export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
       username: process.env.TYPE_ORM_USERNAME,
       password: process.env.TYPE_ORM_PASSWORD,
       database: process.env.TYPE_ORM_DATABASE,
-      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      entities: [__dirname + '/../**/*.entity.{js,ts}'], // entities: [Score],
       synchronize: env.get<boolean>('TYPE_ORM_SYNCHRONIZE'),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }
 
     logger.log(`>>> TypeOrmConfig is connecting to server {${options.type}, ${options.host}, ${options.port}, ${options.database}}`);
