eb4e46f89cca85744132b9818fc60777ad8bfb89
tn9th8 <tn9th8@gmail.com>
2025-07-02 11:34:21 +0700

    cors production

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -1,5 +1,5 @@
 export const corsConfig = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://gcore-app.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     credentials: true,
